89e0c9f0d0f9d3909a7575069eea0de935b0e384
bilfash <bilfashhifnie@outlook.com>
2016-05-19 18:29:25 +0700

    reinit

--- resources/views/layout/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- footer content -->
-<footer>
-    <div class="pull-right">
-        PayIt! || by Mobile Innovation Studio
-    </div>
-    <div class="clearfix"></div>
-</footer>
-<!-- /footer content -->
